0310f5e5e4992279ec7577c505106a103f3b0aa3
EKaizen2 <55081970+EKaizen2@users.noreply.github.com>
2021-11-01 01:30:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 # %%writefile app.py%
 import streamlit as st
 import pickle
-# import openpyxl
-# import xlrd
+import openpyxl
+import xlrd
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
@@ -24,39 +24,38 @@ def main():
     # display the front end aspect
     st.markdown(html_temp, unsafe_allow_html=True)
     default_value_goes_here = ""
-    short_passing = st.number_input("Please enter the players Short Passing Attribute", 0, 100000000, 0)
-    ball_control = st.number_input("Please enter the players Ball Control Attribute", 0, 100000000, 0)
-    reactions = st.number_input("Please enter the players Reactions Attribute", 0, 100000000, 0)
-    balance = st.number_input("Please enter the players Balance Attribute", 0, 100000000, 0)
-    stamina = st.number_input("Please enter the players Stamina Attribute", 0, 100000000, 0)
+#     short_passing = st.number_input("Please enter the players Short Passing Attribute", 0, 100000000, 0)
+#     ball_control = st.number_input("Please enter the players Ball Control Attribute", 0, 100000000, 0)
+#     reactions = st.number_input("Please enter the players Reactions Attribute", 0, 100000000, 0)
+#     balance = st.number_input("Please enter the players Balance Attribute", 0, 100000000, 0)
+#     stamina = st.number_input("Please enter the players Stamina Attribute", 0, 100000000, 0)
 
 #     uploaded_file = st.file_uploader("Choose a XLSX file", type="xlsx")
 
-#     global dataframe
-#     if uploaded_file:
-#         df = pd.read_excel(uploaded_file)
-#         dataframe = df
+    global dataframe
+    if uploaded_file:
+        df = pd.read_excel(uploaded_file)
+        dataframe = df
         # st.dataframe(df)
         # st.table(df)
 
-    attributes = [short_passing, ball_control,reactions, balance, stamina]
+#     attributes = [short_passing, ball_control,reactions, balance, stamina]
     #
     result = ""
     #
     # # Display Books
     if st.button("Predict"):
-#       arr = dataframe.columns
+      arr = dataframe.columns
 
-#       for i in arr:
-#           notnull = dataframe[i][dataframe[i].notnull()]
-#           min = notnull.min()
-#           dataframe[i].replace(np.nan, min, inplace=True)
+      for i in arr:
+          notnull = dataframe[i][dataframe[i].notnull()]
+          min = notnull.min()
+          dataframe[i].replace(np.nan, min, inplace=True)
 
       scaler = StandardScaler()
-      scaler.fit([attributes])
-      featureshost = scaler.transform([attributes])
+      scaler.fit(dataframe)
+      featureshost = scaler.transform(dataframe)
       prediction = model.predict(featureshost)
-
       result = prediction
       st.write(result)
 
